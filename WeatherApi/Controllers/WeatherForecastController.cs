c9a947a2ae2902620e0cf4f9682e739184ebb51c
Quan Do <Q.Do@eml.com.au>
2025-11-18 11:08:50 +0700

    Revert "Update WeatherForecastController.cs"
    This reverts commit c8dc5a5d4d7ea269b5686cac015bc5be428b6f20.

--- WeatherApi/Controllers/WeatherForecastController.cs
+++ WeatherApi/Controllers/WeatherForecastController.cs
@@ -24,10 +24,9 @@ public class WeatherForecastController : ControllerBase
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-21, 56),
+            TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
     }
 }
-
